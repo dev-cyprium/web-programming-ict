e80949d22ac3f286f7f9277f8c60259f60feb83f
Stefan Kupresak <stefan_vg@hotmail.com>
2018-02-04 16:12:39 +0100

    Add favicon to the project

--- index.php
+++ index.php
@@ -11,9 +11,15 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+    <!-- My Own CSS -->
     <link rel="stylesheet" href="style.css">
 
+    <!-- Mobile configuration -->
     <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/ico" href="/favicon.ico">
   </head>
   <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
