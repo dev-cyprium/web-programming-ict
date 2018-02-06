19b085bc0423f101fba4b3065a03cc0ec9e9fdd0
Stefan Kupresak <stefan_vg@hotmail.com>
2018-02-06 03:35:17 +0100

    Add navigation

--- templates/_navigation.php
+++ templates/_navigation.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top border-bottom-black">
   <a class="navbar-brand" href="#">
-    Music Store
+    Choral
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdown" aria-controls="navbarDropdown" aria-expanded="false" aria-label="Toggle navigation" >
     <span class="navbar-toggler-icon"></span>
