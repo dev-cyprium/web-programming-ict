239bb3d4905437d0c20a7904647dceff090a4674
Stefan Kupresak <stefan_vg@hotmail.com>
2018-02-13 14:43:11 +0100

    Add styles for the store page

--- store.php
+++ store.php
@@ -9,5 +9,22 @@
   </head>
   <body>
     <?php include_once("templates/_navigation.php"); ?>
+    <div class='container push-from-top'>
+      <div class='row'>
+        <div class='col-lg-4'>
+          <h1>Categories</h1>
+          <ul class='list-group'>
+            <li class='list-group-item d-flex justify-content-between align-items-center'>
+              Guitars
+              <span class='badge badge-green badge-pill normal-font'>20</span>
+            </li>
+            <li class='list-group-item d-flex justify-content-between align-items-center'>
+              Bass Guitars
+              <span class='badge badge-green badge-pill normal-font'>8</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
