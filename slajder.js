59bbf3f2878c3e3adefeb3a235571e61d5d787f9
Stefan Kupresak <stefan_vg@hotmail.com>
2018-03-07 22:14:47 +0100

    Finished slider

--- slajder.js
+++ slajder.js
@@ -1,10 +1,12 @@
 (function($) {
-
+  /*
+    @author Kupresak Stefan
+    @version 1.0.0
+    @name MySlider
+  */
   var dimensions;
   var currentImage;
-  var $container;
-  var opts;
-  var dime
+  var $container, labels, opts;
 
   var methods = {
     init: init,
@@ -21,10 +23,12 @@
     }
   }
 
-  function init() {
+  function init(config) {
     opts = {
       speed: 2500
     }
+    labels = config.labels || [];
+
     $container = $("<div>");
     $container.addClass('image-container').appendTo(this);
     this.find('img').appendTo($container);
@@ -34,18 +38,35 @@
     $container.css({
       width: 3*w + "px",
       height: h + "px",
-      transition: "all 1s ease-out"
+      transition: "all 0.5s linear"
     });
     currentImage = this.find('img').eq(0);
     this.find('img').show();
+
+    var heading = labels[0].text;
+    $label = $(`<h1>${heading}</h1>`);
+
+    this.append($label);
+    $label.animate({
+      bottom: 0
+    }, 1000);
+
     return this;
   }
 
   function next() {
     var nextImage = getNext();
     var slideTo   = nextImage.position().left;
+    var height    = $label.height();
     $container.css('transform', `translate3d(-${slideTo}px, 0, 0)`)
-    setTimeout(function() { currentImage = nextImage }, 1000);
+    $label.css("bottom", `-${2*height+5}px`);
+    $label.text( labels[nextImage.index()].text );
+    setTimeout(function() {
+      $label.animate({
+        bottom: 0
+      }, 1000);
+      currentImage = nextImage
+    }, 500);
   }
 
   function getNext() {
@@ -55,7 +76,13 @@
 
 
 $(document).ready(function() {
-  var slider = $(".my-slider").slider();
+  var slider = $(".my-slider").slider({
+    labels: [
+      {text: "Professional pianos"},
+      {text: "Awesome guitars"},
+      {text: "Top quality drums"}
+    ]
+  });
   $("#next").click(function() {
     slider.slider('next');
   })
